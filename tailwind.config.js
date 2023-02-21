8cd05ed6fc0a28a0641f31174846cdfbbbef395e
Abdul Rehman <mehars.6925@gmail.com>
2023-02-21 18:08:16 +0500

    configured tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {},
+  plugins: [],
+};
