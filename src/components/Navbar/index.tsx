8f5af9f7252c780517317ff099d8de9d73b0e8e8
Abdul Rehman <mehars.6925@gmail.com>
2023-02-22 01:29:17 +0500

    updated.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -6,12 +6,17 @@ export default function Navbar() {
     return (
         <div className="p-0 px-6 relative flex items-center justify-between bg-white shadow-lg">
             <div>
-                <Link href={'#'} className="max-4 py-3 inline-block">
+                <Link href={'#'} className="max-4 px-6 py-3 inline-block">
                     <Image className="inline-block" height={30} quality={100} alt="Abdul Rehman - Software Engineer" src={HeaderImage} />
                 </Link>
                 <ul className="mx-4 border-l-2 px-6 inline-block">
-                    <Link href="#">Home</Link>
-                    <Link href="#">Home</Link>
+                    <Link className="mx-3 opacity-75 text-sm border-b-2 border-transparent duration-500 hover:border-red-300 hover:opacity-100" href="#">
+                        Home
+                    </Link>
+                    <Link className="mx-3 opacity-75 text-sm border-b-2 border-transparent duration-500 hover:border-red-300 hover:opacity-100" href="#">
+                        About Me
+                    </Link>
+                    
                 </ul>
             </div>
             <div>
