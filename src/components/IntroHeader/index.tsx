87090b186bff535be3d8a139f586bd6c0fcdf450
Abdul Rehman <mehars.6925@gmail.com>
2023-02-22 02:41:15 +0500

    updated.

--- src/components/IntroHeader/index.tsx
+++ src/components/IntroHeader/index.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import ForegroundImage from '../../assets/images/myself_bg.png';
+import WaterMark from '../../assets/images/watermark.png';
 
 export default function IntroHeader() {
     return (
         <div className='relative'>
-            <Image className='absolute right-0' height={600} src={ForegroundImage} alt="Abdul Rehman - Full Stack Developer" />
+            <Image className='absolute left-10 opacity-5 z-20 top-5' height={200} src={WaterMark} alt="Abdul Rehman - Full Stack Developer" />
+            <Image priority className='absolute right-0' height={520} src={ForegroundImage} alt="Abdul Rehman - Full Stack Developer" />
         </div>
     );
 }
