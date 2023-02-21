b1401ccbb71e9433a7b4732c48333cd09458a227
Abdul Rehman <mehars.6925@gmail.com>
2023-02-22 01:17:22 +0500

    updated the favicon

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,15 +4,15 @@ import HeaderImage from '../../assets/images/headerLogo.png';
 
 export default function Navbar() {
     return (
-        <div className="p-0 px-6 flex items-center justify-between bg-white shadow-lg">
+        <div className="p-0 px-6 relative flex items-center justify-between bg-white shadow-lg">
             <div>
-                <Link href={'#'}>
-                    <Image className="inline-block" height={70} quality={100} alt="Abdul Rehman - Software Engineer" src={HeaderImage} />
+                <Link href={'#'} className="max-4 py-3 inline-block">
+                    <Image className="inline-block" height={30} quality={100} alt="Abdul Rehman - Software Engineer" src={HeaderImage} />
                 </Link>
-            </div>
-            <div className="text-left">
-                <Link href="#">Home</Link>
-                <Link href="#">Home</Link>
+                <ul className="mx-4 border-l-2 px-6 inline-block">
+                    <Link href="#">Home</Link>
+                    <Link href="#">Home</Link>
+                </ul>
             </div>
             <div>
                 Contact
