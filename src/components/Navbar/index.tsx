9b2c242ce84f76e2fb4368a2461c365e6b5df9d9
Abdul Rehman <mehars.6925@gmail.com>
2023-02-23 01:25:39 +0500

    fixed errors

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import HeaderImage from "../../assets/images/headerLogo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export default function Navbar() {
   return (
     <div className="p-0 px-6 z-50 fixed w-full flex items-center justify-between bg-white shadow-lg">
       <div>
-        <Link href={"#"} className="max-4 px-6 py-3 inline-block">
+        <Link href={"#"} className="px-6 py-3 sm:px-0 inline-block">
           <Image
             className="inline-block"
             height={30}
@@ -15,7 +17,7 @@ export default function Navbar() {
             src={HeaderImage}
           />
         </Link>
-        <ul className="mx-4 border-l-2 px-6 inline-block">
+        <ul className="mx-4 border-l-2 px-6 inline-block sm:hidden">
           <li className="inline-block">
             <Link
               className="mx-3 opacity-75 text-sm border-b-2 border-transparent duration-500 hover:border-red-300 hover:opacity-100"
@@ -34,7 +36,15 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
-      <div>Contact</div>
+      <div>
+        <Link
+          className="hidden sm:inline-block"
+          href={"#"}
+          onClick={(event) => event.preventDefault()}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </Link>
+      </div>
     </div>
   );
 }
