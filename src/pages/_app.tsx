8b1245a3d16183d6d3aed89ca1fdfe4f47ddffe2
Abdul Rehman <mehars.6925@gmail.com>
2023-06-14 07:59:26 +0500

    configured poppins as default font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { Poppins } from "@next/font/google";
+
+const popins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component className="2xl:scale-150 transform-gpu" {...pageProps} />;
+  return (
+    <main className={`2xl:scale-150 transform-gpu ${popins.className}`}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
